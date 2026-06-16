8cdf8a3d39c92004259851c99b8b2c3e1276cb68
douglasvcsilva-source <douglas.vcsilva@aluno.edu.es.gov.br>
2026-06-16 12:00:38 -0300

    Create index.php

---
+++ aula 01/php/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html lanf="pt-br">
+    <head>  
+        <meta charset ="UTF-8">
+        <title>Aula PHP</title>
+    </head>
+    <body>
+        <?php
+            echo "<h1>Hello <br> World<h1>";
+
+        ?>
+    </body>
+</html>
