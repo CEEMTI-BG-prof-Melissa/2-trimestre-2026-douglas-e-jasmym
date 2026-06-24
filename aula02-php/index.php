79c8dcc4e73ff4bc4a9cd7fc0e45c61144df2f0c
douglasvcsilva-source <douglas.vcsilva@aluno.edu.es.gov.br>
2026-06-24 11:58:16 -0300

    Add files via upload

---
+++ aula02-php/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-bt">
+    <head>
+        <meta charset="UTF-8">
+        <tittle> Aula 02 HTML </tittle>
+    </head>
+    <body>
+
+    <?php
+        $nome1="Douglas";
+        $nome2="Jasmym";
+        $idade1="16";
+        $idade2="16";
+
+        echo "meu nome é" .$nome2 . "eu tenho" .$idade2  
+
+    ?>    
+    </body>
+</html>
